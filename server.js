27784b107261c018bbb0a95c41092efcec8fe91b
ozer619 <prathameshd419@gmail.com>
2021-09-05 02:16:07 +0530

    donw with basic layout

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const ejs = require('ejs');
+
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
