fe470433452d296cdedabf7b55de8642c843ffaa
ozer619 <prathameshd419@gmail.com>
2021-09-05 17:45:49 +0530

    added gifs

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const ejs = require('ejs');
-
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/bmi', (req, res) => {
+  res.render('bmi');
+});
+
+app.get('/underweight', (req, res) => {
+  res.render('bmi');
+});
+
+app.get('/healthy', (req, res) => {
+  res.render('bmiCategories/healthy');
+});
+
+app.get('/overweight', (req, res) => {
+  res.render('bmi');
+});
+
+
+
+app.post('/bmi', (req, res) => {
+  const height = req.body.height;
+  const weight = req.body.weight;
+
+  const bmi = weight/(height*height);
+
+  console.log(bmi);
+
 });
 
 app.listen(port, () => {
